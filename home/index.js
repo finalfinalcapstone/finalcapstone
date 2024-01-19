77fd839b6b591a832e22201b536d202b61d46424
Juan11211 <avilesjuan628@gmail.com>
2024-01-19 10:49:11 -0500

    working on homepage

---
+++ home/index.js
@@ -0,0 +1,72 @@
+"use strict"
+
+// run when page loads
+window.onload = init; 
+
+function init(){
+    var addBtn = document.getElementById("addBtn");
+    addBtn.onclick = onAddBtnClicked;
+
+    var subBtn = document.getElementById("subBtn");
+    subBtn.onclick = onSubBtnClicked;
+
+    var multiplyBtn = document.getElementById("multiplyBtn");
+    multiplyBtn.onclick = onMultiplyBtn;
+
+    var divisionBtn = document.getElementById("divisionBtn");
+    divisionBtn.onclick = onDivisionBtn;
+}
+
+// add button -- 
+function onAddBtnClicked(){
+    var num1Text = document.getElementById("number1Text");
+    var num2Text = document.getElementById("number2Text");
+    var answerField = document.getElementById("answerField");
+
+   var number1 = Number(num1Text.value);
+    var number2 = Number(num2Text.value);
+    
+    var sum = number1 + number2;
+    answerField.value = sum;
+}
+
+// sub button -- 
+function onSubBtnClicked(){
+    var num1Text = document.getElementById("number1Text");
+    var num2Text = document.getElementById("number2Text");
+    var answerField = document.getElementById("answerField");
+
+    var number1 = Number(num1Text.value);
+    var number2 = Number(num2Text.value);
+    
+    var sum = number1 - number2;
+    answerField.value = sum;
+}
+
+function onMultiplyBtn(){
+    var num1Text = document.getElementById("number1Text");
+    var num2Text = document.getElementById("number2Text");
+    var answerField = document.getElementById("answerField");
+
+    var number1 = Number(num1Text.value);
+    var number2 = Number(num2Text.value);
+    
+    var sum = number1 * number2;
+
+    answerField.value = sum;
+}
+
+function onDivisionBtn(){
+    var num1Text = document.getElementById("number1Text");
+    var num2Text = document.getElementById("number2Text");
+    var answerField = document.getElementById("answerField");
+
+    var number1 = Number(num1Text.value);
+    var number2 = Number(num2Text.value);
+    
+    var sum = number1 / number2;
+    
+    answerField.value = sum;
+}
+
+
